15e7da9559d0e7743cacc401be4f97c8e924bc25
mohammad <m.altamimi96@outlook.com>
2026-08-24 03:21:22 +1000

    Start food loops in opposite directions

--- feast-core/assets/feast-core.js
+++ feast-core/assets/feast-core.js
@@ -1,9 +1,12 @@
 ( function () {
 	'use strict';
+	var syncTimer = null;
+	var syncAttempts = 0;
 
 	function configureFoodCarousels( root ) {
 		var scope = root && root.querySelectorAll ? root : document;
 		var carousels = scope.querySelectorAll( '.elementor-element-hmca1001 .swiper, .elementor-element-hmca1002 .swiper' );
+		var configured = 0;
 
 		carousels.forEach( function ( carousel ) {
 			var swiper = carousel.swiper;
@@ -17,17 +20,30 @@
 			swiper.params.autoplay.pauseOnMouseEnter = false;
 			swiper.params.autoplay.reverseDirection = Boolean( carousel.closest( '.elementor-element-hmca1001' ) );
 			swiper.update();
-			if ( swiper.autoplay && swiper.autoplay.running ) {
+			if ( swiper.autoplay ) {
 				swiper.autoplay.stop();
 				swiper.autoplay.start();
 			}
+			configured += 1;
 		} );
+
+		return configured;
 	}
 
 	function scheduleCarouselSync() {
-		window.setTimeout( function () {
-			configureFoodCarousels( document );
-		}, 400 );
+		window.clearTimeout( syncTimer );
+		syncTimer = window.setTimeout( function syncCarousels() {
+			if ( configureFoodCarousels( document ) >= 2 ) {
+				document.documentElement.classList.add( 'feast-food-loop-ready' );
+				syncAttempts = 0;
+				return;
+			}
+
+			syncAttempts += 1;
+			if ( syncAttempts < 40 ) {
+				syncTimer = window.setTimeout( syncCarousels, 50 );
+			}
+		}, 0 );
 	}
 
 	if ( document.readyState === 'loading' ) {
